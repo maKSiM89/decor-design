802c7015df877240833a627f90623118f904b27d
maKSiMka <maksim4uk@gmail.com>
2016-12-28 19:17:55 +0200

    Gallery

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,53 +1,85 @@
 ;(function () {
+	var activeClass = 'active';
+
 	var init = function () {
 		__attachEvents();
+	},
+
+	__attachEvents = function () {
+		$( '.gallery-items img' ).on( 'click', handleShowGallery );
+		$( '.close-popup' ).on( 'click', handleHideGallery );
+		$( document ).on( 'click', '.content .prev', handleClickPrevButton );
+		$( document ).on( 'click', '.content .next', handleClickNextButton );
 	};
+
+	function handleShowGallery() {
+		var index = $( '.gallery-items img' ).index( this );
+		gallery.show( index );
+	}
+
+	function handleHideGallery() {
+		gallery.hide();
+	}
+
+	function handleClickPrevButton() {
+		gallery.prev();
+	}
+
+	function handleClickNextButton() {
+		gallery.next();
+	}
+
+	// gallery object
+	var gallery = (function () {
+		var obj = $( '.gallery' ),
+			popup = $( '.gallery-popup' ),
+			items = obj.find( '.box img' ),
+			slideLine = popup.find( '.slide' ),
+			itemsCount = items.length,
+			currentIndex = 0;
+
+		return {
+			show: _show,
+			hide: _hide,
+			next: _next,
+			prev: _prev
+		};
+
+		function _show( index ) {
+			var img = $( items[index] ),
+				imgSrc = img.attr('src'),
+				slideImg = slideLine.find( 'img' );
+
+			popup.show();
+			currentIndex = index;
+			slideImg.attr( 'src', imgSrc );
+		}
+
+		function _hide() {
+			popup.hide();
+			currentIndex = 0;
+		}
+
+		function _next() {
+			if (currentIndex >= itemsCount - 1) {
+				currentIndex = 0;
+			} else {
+				currentIndex++;
+			}
+			_show( currentIndex );
+		}
+
+		function _prev() {
+			if (currentIndex <= 0) {
+				currentIndex = itemsCount - 1;
+			} else {
+				currentIndex--;
+			}
+			_show( currentIndex );
+		}
+	})();
 
 	$( document ).ready( function () {
 		init();
 	});
 })();
-
-
-
-function openModal() {
-	document.getElementById('myModal').style.display = "block";
-}
-
-function closeModal() {
-	document.getElementById('myModal').style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-	showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-	showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-	var i,
-		slides = document.getElementsByClassName("mySlides"),
-		dots = document.getElementsByClassName("demo"),
-		captionText = document.getElementById("caption");
-
-	if (n > slides.length) {
-		slideIndex = 1;
-	}
-	if (n < 1) {
-		slideIndex = slides.length
-	}
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" active", "");
-	}
-	slides[slideIndex-1].style.display = "block";
-	dots[slideIndex-1].className += " active";
-	captionText.innerHTML = dots[slideIndex-1].alt;
-}
